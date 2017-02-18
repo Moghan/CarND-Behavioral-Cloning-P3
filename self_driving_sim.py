97f6b7ca01b8c633bc8bfe4734e26817299a7380
Anders Kensby <anders.kensby@gmail.com>
2017-02-18 16:34:02 +0100

    fix typo

--- self_driving_sim.py
+++ self_driving_sim.py
@@ -95,7 +95,7 @@ print ('Imported Keras')
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape = (160, 320, 3)))
-model.add(Crapping2D(crapping=((70, 25), (0, 0)))
+model.add(Crapping2D(crapping=((70, 25), (0, 0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = 'relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = 'relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = 'relu'))
