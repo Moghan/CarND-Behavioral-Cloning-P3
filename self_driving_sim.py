949f6189ecb90e5ae641ec4c47a2a0b801c88004
Anders Kensby <anders.kensby@gmail.com>
2017-02-18 22:41:55 +0100

    fix generator

--- self_driving_sim.py
+++ self_driving_sim.py
@@ -1,86 +1,12 @@
 import csv
 import cv2
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
-
-def generator(samples, batch_size=32):
-	num_samples = len(samples)
-
-	while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
-
-def load_data():
-	print("=================================")
-	print ('Loading data...')
-	images = []
-	steering_angles = []
-
-	recorded_data_file = 'recorded_data/driving_log.csv'
-
-	list = []
-
-	with open(recorded_data_file) as csvfile:
-	    reader = csv.reader(csvfile)
-	    for row in reader:
-	    	list.append(row)
-
-	
-
-	path = "recorded_data/IMG/"
-
-
-	for row in list:
-		# get all images center, left and right
-		full_source_path = row[0]
-		img_file_name = full_source_path.split('\\')[-1]
-		image = cv2.imread(path + img_file_name)
-		images.append(image)
-
-		full_source_path = row[1]
-		img_file_name = full_source_path.split('\\')[-1]
-		image = cv2.imread(path + img_file_name)
-		images.append(image)
-
-		full_source_path = row[2]
-		img_file_name = full_source_path.split('\\')[-1]
-		image = cv2.imread(path + img_file_name)
-		images.append(image)
-
-		# get all labels (steering angles)
-		steering_angle = float(row[3])
-		correction = 0.2
-		
-		steering_angles.append(steering_angle)
-		steering_angles.append(steering_angle + correction)
-		steering_angles.append(steering_angle - correction)
-
-	print ('--------------------------------')
-	print ('Rawdata loaded:')
-	print ('%i Images' % len(images))
-	print ('%i Labels' % len(steering_angles))
-	print ('--------------------------------')
-
-	return images, steering_angles
+CSV_file_name = 'recorded_data/driving_log.csv'
 
 def augmentation(images, steering_angles):
-	print("=================================")
-	print ('Augmenting data...')
 	augmented_images = []
 	augmented_steering_angles = []
 
@@ -93,26 +19,113 @@ def augmentation(images, steering_angles):
 		augmented_images.append(flipped_image)
 		augmented_steering_angles.append(flipped_angle)
 
-	print ('---------------------------')
-	print ('Data augmented:')
-	print ('%i Images' % len(augmented_images))
-	print ('%i Labels' % len(augmented_steering_angles))
-	print ('---------------------------')
-
 	return augmented_images, augmented_steering_angles
 
+def load_batch_data(batch_samples):
+	images = []
+	angles = []
 
-images, steering_angles = load_data()
-augmented_images, augmented_steering_angles = augmentation(images, steering_angles)
+	for batch_sample in batch_samples:
+		center_angle = float(batch_sample[3])
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_steering_angles)
+		#center camera
+		name = './recorded_data/IMG/'+batch_sample[0].split('\\')[-1]
+		center_image = cv2.imread(name)
+		images.append(center_image)
+		# print('name' + name)
+		# print(center_image.shape)
 
-# X_train = np.array(images)
-# y_train = np.array(steering_angles)
+		#left camera
+		name = './recorded_data/IMG/'+batch_sample[1].split('\\')[-1]
+		left_image = cv2.imread(name)
+		images.append(center_image)
 
-train_generator = generator(X_train)
-valid_generator = generator(y_train)
+		#right camera
+		name = './recorded_data/IMG/'+batch_sample[2].split('\\')[-1]
+		right_image = cv2.imread(name)
+		images.append(center_image)
+
+		#append center, left and right angle
+		correction = 0.2
+		angles.append(center_angle)
+		angles.append(center_angle + correction)
+		angles.append(center_angle - correction)
+
+	return images, angles
+
+
+
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+	while 1: # Loop forever so the generator never terminates
+		shuffle(samples)
+		for offset in range(0, num_samples, batch_size):
+			batch_samples = samples[offset:offset+batch_size]
+
+			images = []
+			angles = []
+			for batch_sample in batch_samples:
+				# center_angle = float(batch_sample[3])
+
+				# #center camera
+				# name = './recorded_data/IMG/'+batch_sample[0].split('\\')[-1]
+				# center_image = cv2.imread(name)
+				# images.append(center_image)
+				# # print('name' + name)
+				# # print(center_image.shape)
+
+				# #left camera
+				# name = './recorded_data/IMG/'+batch_sample[1].split('\\')[-1]
+				# left_image = cv2.imread(name)
+				# images.append(center_image)
+
+				# #right camera
+				# name = './recorded_data/IMG/'+batch_sample[2].split('\\')[-1]
+				# right_image = cv2.imread(name)
+				# images.append(center_image)
+
+				# #append center, left and right angle
+				# correction = 0.2
+				# angles.append(center_angle)
+				# angles.append(center_angle + correction)
+				# angles.append(center_angle - correction)
+
+				#load images and angles
+				images, angles = load_batch_data(batch_samples)
+
+				#augment 
+				# images, angles = augmentation(images, angles)
+
+
+			X_train = np.array(images)
+			y_train = np.array(angles)
+
+			yield shuffle(X_train, y_train)
+
+def load_CSV(file_name):
+	list = []
+
+	with open(file_name) as csvfile:
+	    reader = csv.reader(csvfile)
+	    for row in reader:
+	    	list.append(row)
+
+	print('======================================')
+	print('Data statistics\n')
+	print('Number of RAW samples: %i' % len(list))
+	print('Number of images: %i' % (len(list)*3))
+	print('Augmented no images: %i' % (len(list)*6))
+
+	return list
+
+
+samples = load_CSV(CSV_file_name)
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+
+train_generator = generator(train_samples)
+validation_generator = generator(validation_samples)
+
 
 import keras
 from keras.models import Sequential
@@ -125,18 +138,26 @@ print ('Imported Keras')
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape = (160, 320, 3)))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+
+# nvidias end-to-end self-driving-car
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation = 'relu'))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation = 'relu'))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation = 'relu'))
 model.add(Convolution2D(64, 3, 3, activation = 'relu'))
 model.add(Convolution2D(24, 3, 3, activation = 'relu'))
-
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(1))
+
+# model.add(Convolution2D(24, 3, 3, activation = 'relu'))
+# model.add(Flatten())
+# model.add(Dense(50))
+# model.add(Dense(1))
+
+
 model.compile(optimizer='adam', loss='mse')
 # model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*3, validation_data=validation_generator, nb_val_samples=len(validation_samples)*3, nb_epoch=3)
 
 model.save('model.h5')
