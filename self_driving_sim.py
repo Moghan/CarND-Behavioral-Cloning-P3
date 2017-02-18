6dba3921547ea0194bb5714f6bfedcedf347bb57
Anders Kensby <anders.kensby@gmail.com>
2017-02-18 20:29:03 +0100

    add generator

--- self_driving_sim.py
+++ self_driving_sim.py
@@ -3,6 +3,28 @@ import cv2
 import numpy as np
 
 
+def generator(samples, batch_size=32):
+	num_samples = len(samples)
+
+	while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
 def load_data():
 	print("=================================")
 	print ('Loading data...')
@@ -89,6 +111,9 @@ y_train = np.array(augmented_steering_angles)
 # X_train = np.array(images)
 # y_train = np.array(steering_angles)
 
+train_generator = generator(X_train)
+valid_generator = generator(y_train)
+
 import keras
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -111,6 +136,7 @@ model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
 model.save('model.h5')
